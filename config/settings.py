36cac47b60af76b68127d81ec7636d6265a6649b
S.Nesterovich <stas.nester18@mail.ru>
2026-07-09 20:08:32 +0300

    ci\cd

--- config/settings.py
+++ config/settings.py
@@ -24,11 +24,11 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
-STRIPE_TOKEN = os.getenv("STRIPE_TOKEN")
+SECRET_KEY = os.getenv("SECRET_KEY","Iw[ldsfjqw[oeifj")
+STRIPE_TOKEN = os.getenv("STRIPE_TOKEN","dsdaqsdasdacasxcas")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.getenv("DEBUG","True") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
@@ -93,11 +93,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("DB_NAME","TEST"),
+        "USER": os.getenv("DB_USER","I_User"),
+        "PASSWORD": os.getenv("DB_PASSWORD","123456"),
+        "HOST": os.getenv("DB_HOST","localhost"),
+        "PORT": os.getenv("DB_PORT",5432),
     }
 }
 
@@ -170,9 +170,9 @@ CELERY_TASK_TRACK_STARTED = True
 # Максимальное время на выполнение задачи
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "True"
+EMAIL_HOST = os.getenv("EMAIL_HOST","smtp.gmail.com")
+EMAIL_PORT = os.getenv("EMAIL_PORT","587")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER","I_User")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD","123456789")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS","True") == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL","True") == "True"
